fec87fc890ce6418c7815ca0ee5e050663b747a9
AfriLove Team <afriloveteam@gmail.com>
2026-04-02 11:15:02 +0100

    🚀 Ultra auto deploy

--- start-server.mjs
+++ start-server.mjs
@@ -4,20 +4,10 @@ import { fileURLToPath } from 'url';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
-
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-app.use(express.static(path.join(__dirname, 'public')));
-
-app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'public/index.html'));
-});
-
-app.use((req, res) => {
-  res.sendFile(path.join(__dirname, 'public/index.html'));
-});
-
-app.listen(PORT, () => {
-  console.log('Server running on http://127.0.0.1:' + PORT);
-});
+app.use('/assets', express.static(path.join(__dirname,'public/assets')));
+app.get('/', (req,res)=>res.sendFile(path.join(__dirname,'public/index.html')));
+app.get('/*',(req,res)=>res.sendFile(path.join(__dirname,'public/index.html')));
+app.listen(PORT,()=>console.log('Server running on http://127.0.0.1:'+PORT));
